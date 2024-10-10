e0d9a621cc948b7e6398ccf910017675ea15b867
Nabilm100 <nabilm402@gmail.com>
2024-10-10 23:42:28 +0300

    hwa da

--- index2.js
+++ index2.js
@@ -1,1 +1,17 @@
-console.log("a7aaaa")
+const names = ['nabil','ali','ahmed']
+
+function fun(arr,cb){
+    arr.forEach(element => {
+        cb(element);
+        
+        
+    });
+    console.log('load');
+
+}
+
+
+fun(names,(name)=>{
+    console.log(name);
+
+})
